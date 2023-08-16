6b358c3f2bf431c6e28756bfa64fb0e5aff03095
Hellga2023 <helllga2018@yandex.ru>
2023-08-16 10:30:15 +0400

    feature/RE4M eslint plugins added, sass added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,18 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 11,
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/ban-ts-comment': 1,
-  },
-}
+    env: {
+        browser: true,
+        es2020: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'airbnb'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 11,
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        '@typescript-eslint/ban-ts-comment': 1,
+        indent: ['error', 4],
+        'react/jsx-indent': ['error', 4],
+    },
+};
