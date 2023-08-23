db3731b1e04c71c88a87d9425ca6964df1b574ec
Markeev Andrew <mccorn13@gmail.com>
2023-08-23 15:24:37 +0300

    add HTTPTransport

---
+++ packages/client/src/utils/HTTPTransport/HTTPTransport.tsx
@@ -0,0 +1,74 @@
+import { XHR_METHODS_TYPES, someObject } from '../../const/types';
+
+const METHODS = XHR_METHODS_TYPES;
+
+function queryStringify(data: someObject) {
+    let result = '';
+
+    Object.keys(data).forEach((key) => {
+        result += `${result ? '&' : '?'}${key}=${data[key]}`;
+    });
+
+    return result;
+}
+
+type HTTPMethod = (url: string, options?: someObject) => Promise<unknown>;
+
+export class HTTPTransport {
+    get: HTTPMethod = (url, options = {}) =>
+        this.request(url + queryStringify(options.data), { ...options, method: METHODS.GET });
+
+    put: HTTPMethod = (url, options = {}) =>
+        this.request(url, {
+            ...options,
+            method: METHODS.PUT,
+        });
+
+    post: HTTPMethod = (url, options = {}) =>
+        this.request(url, { ...options, method: METHODS.POST });
+
+    delete: HTTPMethod = (url, options = {}) =>
+        this.request(url, { ...options, method: METHODS.DELETE });
+
+    // eslint-disable-next-line class-methods-use-this
+    request = (url: string, options: someObject = { method: METHODS.GET }) => {
+        const { method, data, headers = {} } = options;
+
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+
+            xhr.open(method, url);
+            xhr.withCredentials = true;
+
+            Object.keys(headers).forEach((key) => xhr.setRequestHeader(key, headers[key]));
+
+            xhr.onload = () => {
+                resolve(xhr);
+            };
+
+            xhr.onabort = reject;
+            xhr.onerror = reject;
+            xhr.ontimeout = reject;
+
+            if (method === METHODS.GET || !data) {
+                xhr.send();
+            } else {
+                xhr.send(data);
+            }
+        });
+    };
+}
+
+export function fetchWithRetry(url: string, options: someObject) {
+    let { retries = 1 } = options;
+
+    let response = null;
+    const xhr = new HTTPTransport();
+
+    while (retries > 0 && !response) {
+        retries -= 1;
+        response = xhr.request(url, options);
+    }
+
+    return response;
+}
