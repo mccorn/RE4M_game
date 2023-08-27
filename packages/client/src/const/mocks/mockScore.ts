cd0673f0f165161bd0ae3c7899c4ff56eedb185c
Konstantin Soluianov <dr.chtec@yandex.ru>
2023-08-27 21:06:31 +0400

    add markup for leaderboard

---
+++ packages/client/src/const/mocks/mockScore.ts
@@ -0,0 +1,44 @@
+const score = [
+    {
+        name: 'solko',
+        score: 1000,
+    },
+    {
+        name: 'momo',
+        score: 999,
+    },
+    {
+        name: 'momo',
+        score: 999,
+    },
+    {
+        name: 'momo',
+        score: 999,
+    },
+    {
+        name: 'momo',
+        score: 999,
+    },
+    {
+        name: 'solko',
+        score: 1000,
+    },
+    {
+        name: 'momo',
+        score: 999,
+    },
+    {
+        name: 'momo',
+        score: 999,
+    },
+    {
+        name: 'momo',
+        score: 999,
+    },
+    {
+        name: 'momo',
+        score: 999,
+    },
+];
+
+export default score;
