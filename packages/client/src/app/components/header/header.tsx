fc7bd987649cd47ded502d712c37b14306586aeb
Konstantin Soluianov <dr.chtec@yandex.ru>
2023-08-28 17:22:02 +0400

    add opportunity change color

--- packages/client/src/app/components/header/header.tsx
+++ packages/client/src/app/components/header/header.tsx
@@ -1,9 +1,11 @@
-import React, { FC, MouseEventHandler } from 'react';
+import React, { FC, MouseEventHandler, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import classNames from 'classnames';
 import Button from '@/app/components/common/button/button';
 import UserInfo from '@/app/components/userInfo/userInfo';
 import Logo from '@/assets/images/logo.svg';
+import Moon from '@/assets/images/moon.svg';
+import Sun from '@/assets/images/sun.svg';
 import { RoutePaths as Paths } from '@/app/router/router';
 import mockUser from '@/const/mocks/mockUser';
 import style from './header.module.scss';
@@ -15,6 +17,8 @@ type THeaderProps = {
 };
 
 const Header: FC<THeaderProps> = ({ isAuthorized }) => {
+    const [imageForChangeColorMode, setimageForChangeColorMode] = useState(Moon);
+
     const logout: MouseEventHandler = () => {
         AuthAPI.logout();
 
@@ -28,6 +32,30 @@ const Header: FC<THeaderProps> = ({ isAuthorized }) => {
         return classNames(style.header__link, activeClass);
     };
 
+    const changeColorMode = () => {
+        if (imageForChangeColorMode === Moon) {
+            setimageForChangeColorMode(Sun);
+
+            document.documentElement.style.setProperty('--backgroundColor', '#000218');
+
+            document.documentElement.style.setProperty('--backgroundColorSecondary', '#344756');
+
+            document.documentElement.style.setProperty('--backgroundInput', '#D2D2D2');
+
+            document.documentElement.style.setProperty('--buttonBackgroundColor', '#595B82');
+        } else {
+            setimageForChangeColorMode(Moon);
+
+            document.documentElement.style.setProperty('--backgroundColor', '#009797');
+
+            document.documentElement.style.setProperty('--backgroundColorSecondary', '#95b1b0');
+
+            document.documentElement.style.setProperty('--backgroundInput', '#fff');
+
+            document.documentElement.style.setProperty('--buttonBackgroundColor', '#324b4b');
+        }
+    };
+
     return (
         <div className={style.header}>
             <div>
@@ -37,6 +65,13 @@ const Header: FC<THeaderProps> = ({ isAuthorized }) => {
             </div>
 
             <div className={style.header__navigation}>
+                <Button buttonStyle="withoutBackGround" size="small" click={changeColorMode}>
+                    <img
+                        className={style.header__changeColorMode}
+                        src={imageForChangeColorMode}
+                        alt="change color mode"
+                    />
+                </Button>
                 <nav className={style.header__links}>
                     <NavLink
                         to={Paths.GAME}
