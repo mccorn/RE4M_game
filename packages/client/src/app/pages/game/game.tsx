12c78b6c828da1e6085597a2e784ccf5b53311f6
Hellga2023 <helllga2018@yandex.ru>
2023-09-01 16:11:10 +0400

    feature/R4SU-29_canvas initial game commit

--- packages/client/src/app/pages/game/game.tsx
+++ packages/client/src/app/pages/game/game.tsx
@@ -1,15 +1,96 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 import style from './game.module.scss';
 import Button from '@/app/components/common/button/button';
+import params from './gameEngine/parameters/gameParameters';
+import GameEngine from './gameEngine/gameEngine';
 
-const Game: FC = () => (
-    <>
-        <div className={style.game}>Game stub</div>
-        <div>
-            <Button text="large" size="large" />
-            <Button text="medium" size="medium" />
-            <Button text="small" size="small" />
+const Game: FC = () => {
+    const ref = useRef<HTMLCanvasElement | null>(null);
+
+    let gameEngine: GameEngine | null = null;
+
+    const [paused, setIsPaused] = useState(false);
+
+    const onKeyDown = (event: KeyboardEvent) => {
+        // console.log('key pressed');
+        // console.log(event);
+        gameEngine && gameEngine.gameControlPressed(event);
+    };
+
+    const checkGameEngine = () => {
+        if (!gameEngine) {
+            const context = (ref.current as HTMLCanvasElement).getContext('2d');
+            if (context) {
+                gameEngine = new GameEngine(context);
+            } else {
+                console.log('no context found');
+            }
+        }
+    };
+
+    const startGame = () => {
+        /* const gameEnded = false;
+        if (gameEnded) {
+            window.removeEventListener('keydown', onKeyDown);
+        } */
+
+        gameEngine && gameEngine.start();
+        window.addEventListener('keydown', onKeyDown);
+    };
+
+    const pauseGame = () => {
+        if (paused) {
+            console.log('in resume');
+            console.log(gameEngine);
+            checkGameEngine();
+            gameEngine && gameEngine.resume();
+            setIsPaused(false);
+        } else {
+            console.log('in pause');
+            console.log(gameEngine);
+            checkGameEngine();
+            gameEngine && gameEngine.pause();
+            setIsPaused(true);
+        }
+    };
+
+    const endGame = () => {
+        gameEngine && gameEngine.finish();
+    };
+
+    useEffect(() => {
+        const context = (ref.current as HTMLCanvasElement).getContext('2d');
+        if (context) {
+            gameEngine = new GameEngine(context);
+            gameEngine.load(); // todo reload page bug
+        } else {
+            console.log('no context found');
+        }
+    }, []);
+
+    return (
+        <div className={style.game}>
+            <div className={style.game__header}>Play game online</div>
+            <div className={style.game__controls}>
+                Game controls: Arrow buttons to move. A button to fire
+            </div>
+            <div>
+                <canvas
+                    ref={ref}
+                    width={params.WIDTH}
+                    height={params.HEIGHT}
+                    className={style.game__canvas}>
+                    the game should be here
+                </canvas>
+            </div>
+            <div className={style.game__buttons}>
+                <Button text="Start game" size="medium" click={startGame} />
+
+                <Button text="Pause game" size="medium" click={pauseGame} />
+
+                <Button text="End game" size="medium" click={endGame} />
+            </div>
         </div>
-    </>
-);
+    );
+};
 export default Game;
---
+++ packages/client/src/app/pages/game/gameEngine/types/trajectory.ts
@@ -0,0 +1,82 @@
+import { TPoint } from './commonTypes';
+
+class Trajectory {
+    private points: TPoint[];
+
+    private segmentIndex = 0;
+
+    private segmentStartTime = 0;
+
+    private delay = 0;
+
+    constructor(points: TPoint[], delay?: number) {
+        this.points = points;
+        if (delay) {
+            this.delay = delay;
+        }
+    }
+
+    private static getSegmentLength = (startPoint: TPoint, endPoint: TPoint): number =>
+        Math.hypot(endPoint.x - startPoint.x, endPoint.y - startPoint.y);
+
+    private getNextCoordinate = (
+        start: number,
+        end: number,
+        time: number,
+        length: number
+    ): number => {
+        console.log(this.points);
+
+        return start + ((end - start) * time) / length; // todo speed coeficient
+    };
+
+    public getCoordinates = (time: number): TPoint => {
+        const startPoint = this.points[this.segmentIndex];
+        const endPoint = this.points[this.segmentIndex + 1];
+        const length = Trajectory.getSegmentLength(startPoint, endPoint);
+        const segmentTime = time - this.segmentStartTime - this.delay;
+        console.log('start');
+        console.log(startPoint);
+        console.log('end');
+        console.log(endPoint);
+        console.log('length');
+        console.log(length);
+        console.log('time');
+        console.log(time); /**/
+
+        const point = {
+            x: this.getNextCoordinate(startPoint.x, endPoint.x, segmentTime, length),
+            y: this.getNextCoordinate(startPoint.y, endPoint.y, segmentTime, length),
+        };
+        // switch segment if needed
+        if (this.shouldSwitchSegment(time - this.delay)) {
+            this.segmentIndex += 1;
+            this.segmentStartTime = time - this.delay;
+        }
+        return point;
+    };
+
+    public getSegmentsNumber = (): number => this.points.length - 1;
+
+    public shouldSwitchSegment = (time: number): boolean => {
+        let length = 0;
+        for (let i = 0; i <= this.segmentIndex; i++) {
+            length += Trajectory.getSegmentLength(this.points[i], this.points[i + 1]);
+        }
+        return time >= length;
+    };
+
+    public shouldMove = (time: number) =>
+        this.segmentIndex < this.getSegmentsNumber() && this.shouldStartMoving(time);
+
+    public shouldStartMoving = (time: number) => time > this.delay;
+
+    public movedOutOfGameField = (time: number): boolean => {
+        // todo calculate if outside boundaries
+        console.log(time);
+        console.log(this.delay);
+        return false;
+    };
+}
+
+export default Trajectory;
