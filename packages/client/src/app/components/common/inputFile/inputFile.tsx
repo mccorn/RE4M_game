62e498c8e58bea1ae23d557227211d07be3018fd
Markeev Andrew <mccorn13@gmail.com>
2023-09-04 07:01:18 +0300

    Чистка стилей inputFile

--- packages/client/src/app/components/common/inputFile/inputFile.tsx
+++ packages/client/src/app/components/common/inputFile/inputFile.tsx
@@ -1,36 +1,17 @@
 import React, { FC } from 'react';
-// import classNames from 'classnames';
-// import style from './inputFile.module.scss';
-import './inputFile.scss';
+import style from './inputFile.module.scss';
 import { someFunction } from '@/const/types';
 
 type TInputFileProps = {
-    // size?: 'large' | 'medium' | 'small';
-    // inputStyle?: 'normal';
-    // isActive?: boolean;
     onChange?: someFunction;
-    // value?: string;
     name?: string;
-    // label?: string;
-    // placeholder?: string;
-    // className?: string;
 };
 
-const InputFile: FC<TInputFileProps> = ({
-    // size = 'medium',
-    // inputStyle = 'normal',
-    // isActive = true,
-    onChange,
-    // value = '',
-    // label = '',
-    name = '',
-    // placeholder,
-    // className,
-}) => (
-    <div className="inputFile">
-        <label className="inputFile_label">
+const InputFile: FC<TInputFileProps> = ({ onChange, name = '' }) => (
+    <div className={style.inputFile}>
+        <label>
             <span>+</span>
-            <input onChange={onChange} className="inputFile_input" type="file" name={name} />
+            <input onChange={onChange} type="file" name={name} />
         </label>
     </div>
 );
