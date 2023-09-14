03845794a6870260a76bdb43a51e1757778d5b5e
Andrew <mc.corn@mail.ru>
2023-09-14 13:23:00 +0400

    Правки ошибки отображения хэдера в зависимости от авторизованности (#39)

--- packages/client/src/app/pages/signin/signin.tsx
+++ packages/client/src/app/pages/signin/signin.tsx
@@ -1,15 +1,21 @@
+/* eslint-disable consistent-return */
 import React, { FC, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import Form from '@/app/components/common/form/form';
 import Button from '@/app/components/common/button/button';
 import Input from '@/app/components/common/input/input';
 import AuthAPI from '@/app/api/AuthAPI';
 import { RoutePaths } from '@/app/router/router';
+import { TResponse } from '@/const/types';
+import utils from '@/utils';
+import { signIn } from '@/app/store/slices/userSlice';
 
 const Signin: FC = () => {
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const handleChangeLogin = (event: React.ChangeEvent<HTMLInputElement>) => {
         setLogin(event.target.value);
@@ -20,16 +26,23 @@ const Signin: FC = () => {
     };
 
     const handleSubmitForm = () => {
-        AuthAPI.login(
-            {
-                login,
-                password,
-            },
-            () => {
-                console.log('in callback');
-                navigate(RoutePaths.GAME);
-            }
-        );
+        AuthAPI.login({
+            login,
+            password,
+        })
+            .then(response => {
+                const status = (response as TResponse)?.status;
+
+                if (status === 200) {
+                    navigate(RoutePaths.GAME);
+
+                    return AuthAPI.getAuthUser();
+                }
+            })
+            .then(response => {
+                const responseData = utils.safeGetData(response);
+                dispatch(signIn(responseData));
+            });
     };
 
     return (
