59e1bc706bb315696823ed241b2cd5377fa2e39d
Markeev Andrew <mccorn13@gmail.com>
2023-09-20 16:19:58 +0300

    Подготовка сервера к ssr. Докрутка клиента

--- packages/client/ssr.tsx
+++ packages/client/ssr.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Provider } from 'react-redux';
+import { store } from './src/app/store/store';
+
 import App from './src/app/components/app/App';
 
 function render() {
-    return renderToString(<App />);
+    return renderToString(
+        <React.StrictMode>
+            <ErrorBoundary fallback={<>Something went wrong</>}>
+                <Provider store={store}>
+                    <App />
+                </Provider>
+            </ErrorBoundary>
+        </React.StrictMode>
+    );
 }
 
 export default { render };
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,48 @@
 import dotenv from 'dotenv';
+import path from 'path';
+import fs from 'fs';
 import cors from 'cors';
 import express from 'express';
-import createClientAndConnect from './db';
+// import createClientAndConnect from './db';
 
 dotenv.config();
 
-const app = express();
-app.use(cors());
-const port = Number(process.env.SERVER_PORT) || 3001;
+async function startServer() {
+    // createClientAndConnect();
 
-createClientAndConnect();
+    const app = express();
+    app.use(cors());
+    const port = Number(process.env.SERVER_PORT) || 3001;
 
-app.get('/', (_, res) => {
-    res.json('👋 Howdy from the server :)');
-});
+    const distPath = path.dirname(require.resolve('client/dist/index.html'));
+    // const srcPath = path.dirname(require.resolve('client'));
+    const ssrClientPath = require.resolve('client/ssr-dist/client.cjs');
 
-app.listen(port, () => {
-    console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
-});
+    app.get('/', (_, res) => {
+        res.json('👋 Howdy from the server :)');
+    });
+
+    app.use('/assets', express.static(path.resolve(distPath, 'assets')));
+
+    app.use('*', async (_, res, next) => {
+        try {
+            const template = fs.readFileSync(path.resolve(distPath, 'index.html'), 'utf-8');
+
+            const { render } = await import(ssrClientPath);
+
+            const appHtml = await render();
+
+            const html = template.replace('<!--ssr-outlet-->', appHtml);
+
+            res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+        } catch (error) {
+            next(error);
+        }
+    });
+
+    app.listen(port, () => {
+        console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
+    });
+}
+
+startServer();
