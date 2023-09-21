5a4934274a22c8258ade75839feacd6b8c603fec
Markeev Andrew <mccorn13@gmail.com>
2023-09-21 08:24:41 +0300

    v1 (work with warning)

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,24 +3,37 @@ import path from 'path';
 import fs from 'fs';
 import cors from 'cors';
 import express from 'express';
+import { JSDOM } from 'jsdom';
 // import createClientAndConnect from './db';
 
 dotenv.config();
+
+const port = Number(process.env.SERVER_PORT) || 3001;
+
+const { window } = new JSDOM('<div id="app"></div>', { url: `http://localhost:${port}` });
+
+global.window = window as unknown as Window & any;
+global.document = window.document;
+global.navigator = window.navigator;
+global.XMLHttpRequest = window.XMLHttpRequest;
+global.DocumentFragment = window.DocumentFragment;
+global.HTMLElement = window.HTMLElement;
+global.FormData = window.FormData;
+global.Image = window.Image;
 
 async function startServer() {
     // createClientAndConnect();
 
     const app = express();
     app.use(cors());
-    const port = Number(process.env.SERVER_PORT) || 3001;
 
     const distPath = path.dirname(require.resolve('client/dist/index.html'));
     // const srcPath = path.dirname(require.resolve('client'));
     const ssrClientPath = require.resolve('client/ssr-dist/client.cjs');
 
-    app.get('/', (_, res) => {
-        res.json('👋 Howdy from the server :)');
-    });
+    // app.get('/', (_, res) => {
+    //     res.json('👋 Howdy from the server :)');
+    // });
 
     app.use('/assets', express.static(path.resolve(distPath, 'assets')));
 
