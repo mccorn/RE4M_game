e9720c99d204d329aadc6ed969bae405bad64a16
Konstantin Soluianov <dr.chtec@yandex.ru>
2023-10-03 20:29:02 +0400

    router

---
+++ packages/client/src/app/components/ssrApp/App.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+const appContent = 'Вот тут будет жить ваше приложение :)';
+
+// eslint-disable-next-line
+test('Example test', async () => {
+    render(<>Вот тут будет жить ваше приложение :)</>);
+    // eslint-disable-next-line
+    expect(screen.getByText(appContent)).toBeDefined();
+});
--- packages/client/ssr.tsx
+++ packages/client/ssr.tsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './src/app/store/store';
 
-import App from './src/app/components/app/App';
+import App from './src/app/components/ssrApp/App';
 
 // eslint-disable-next-line import/prefer-default-export
 export const store = configureStore({
@@ -15,12 +15,12 @@ export const store = configureStore({
     },
 });
 
-export function render() {
+export function render(req: Request) {
     return renderToString(
         <React.StrictMode>
             <ErrorBoundary fallback={<>Something went wrong</>}>
                 <Provider store={store}>
-                    <App />
+                    <App url={req.url} />
                 </Provider>
             </ErrorBoundary>
         </React.StrictMode>
