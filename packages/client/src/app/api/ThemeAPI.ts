14f7afce789430b8dca47c73d647970b8cb56f52
Konstantin Soluianov <dr.chtec@yandex.ru>
2023-10-17 19:11:52 +0400

    added themeApi

---
+++ packages/client/src/app/api/ThemeAPI.ts
@@ -0,0 +1,16 @@
+import HTTPTransport from '@/utils/HTTPTransport/HTTPTransport';
+import BaseAPI, { API_URL } from '../api';
+
+const http = HTTPTransport;
+
+class ThemeAPI extends BaseAPI {
+    url = `${API_URL.LOCALHOST}/theme`;
+
+    switch(data: { theme: string; login?: string }) {
+        return http.put(
+            `${this.url}/switchTheme?theme=${data.theme}${data.login ? `&login=${data.login}` : ''}`
+        ) as Promise<string>;
+    }
+}
+
+export default new ThemeAPI();
